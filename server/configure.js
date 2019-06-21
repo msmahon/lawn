4f887e89081551a417c5425f74dff7b94b197e7e
Mitchell Mahoney <mahoneymitch@gmail.com>
2019-06-21 09:51:58 -0400

    Changing indent.

--- server/configure.js
+++ server/configure.js
@@ -2,6 +2,6 @@ const bodyParser = require('body-parser')
 const api = require('./api')
 
 module.exports = app => {
-  app.use(bodyParser.json())
-  app.use('/api', api)
+	app.use(bodyParser.json())
+	app.use('/api', api)
 }
