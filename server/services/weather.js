9a52962d0523df86d0486abd8134aa170ce6aa11
Mitchell Mahoney <mahoneymitch@gmail.com>
2019-08-08 18:32:25 -0400

    Adding axios.

--- server/services/weather.js
+++ server/services/weather.js
@@ -1,19 +1,10 @@
-const https = require('https');
+const https = require('https')
+const axios = require('axios')
 
 module.exports = class Query {
 	constructor() {
 		let endpoint =  `https://api.openweathermap.org/data/2.5/forecast?zip=29607&appid=${process.env.WEATHER_API_KEY}`
-		https.get(endpoint, (resp) => {
-			let data = ''
-
-			resp.on('data', (chunk) => {
-				data += chunk
-			})
-
-			resp.on('end', () => {
-				console.log(JSON.parse(data))
-			})
-		})
+		
 	}
 
 	getWeather() {
