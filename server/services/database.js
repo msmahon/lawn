377540fe69b227e80a5c8a923f03def8e0ff64ee
Mitchell Mahoney <mahoneymitch@gmail.com>
2019-08-08 18:32:43 -0400

    Adding grass table.

--- server/services/database.js
+++ server/services/database.js
@@ -5,12 +5,14 @@ module.exports = class Query {
 		this.db = Database(database, {verbose: console.log})
 	}
 
+
+
 	resetDatabase() {
 		let drop = "DROP TABLE IF EXISTS lawns;"
-		let create = "CREATE TABLE lawns (name TEXT, data TEXT);"
+		let createLawnTable = "CREATE TABLE lawns (name TEXT, data TEXT);"
 
 		this.db.prepare(drop).run()
-		this.db.prepare(create).run()
+		this.db.prepare(createLawnTable).run()
 
 		this.addLawn('test',JSON.stringify(
 			{
@@ -51,6 +53,45 @@ module.exports = class Query {
 				}
 			}
 		))
+
+		this.createGrassTable()
+	}
+
+	createGrassTable() {
+		let drop = "DROP TABLE IF EXISTS grass_types;"
+		let createGrassTable = `CREATE TABLE grass_types (
+			name TEXT UNIQUE,
+			blade TEXT,
+			color TEXT,
+			texture TEXT,
+			growth TEXT,
+			water TEXT,
+			season TEXT,
+			image_url TEXT
+		);`
+
+		this.db.prepare(drop).run()
+		this.db.prepare(createGrassTable).run()
+
+		let grasses = [
+			{ name: "Bentgrass",			blade: "narrow, flat",			color: "green",					texture: "soft, dense",		growth: "slow",	water: "frequent",		season: "cool", image_url: "#"},
+			{ name: "Bermuda",				blade: "sharp, pointed",		color: "deep green",			texture: "dense",			growth: "slow",	water: "frequent",		season: "warm", image_url: "#"},
+			{ name: "Centipede",			blade: "pointed with notch",	color: "light green",			texture: "soft, dense",		growth: "slow",	water: "infrequent",	season: "warm", image_url: "#"},
+			{ name: "Dichondra",			blade: "round leaves",			color: "pale to bright green",	texture: "dense",			growth: "slow",	water: "frequent",		season: "warm", image_url: "#"},
+			{ name: "Fine Fescue",			blade: "hairlike",				color: "dull or gray-green",	texture: "soft",			growth: "fast",	water: "frequent",		season: "cool", image_url: "#"},
+			{ name: "Kentucky Bluegrass",	blade: "v-shaped, pointed",		color: "darker green",			texture: "soft",			growth: "fast",	water: "average",		season: "cool", image_url: "#"},
+			{ name: "Ryegrass",				blade: "pointed, veined",		color: "dark green",			texture: "soft",			growth: "fase",	water: "average",		season: "cool", image_url: "#"},
+			{ name: "St. Augustine",		blade: "broad with round tip",	color: "dark green",			texture: "coarse, spongey",	growth: "slow",	water: "frequent",		season: "warm", image_url: "#"},
+			{ name: "Tall Fescue",			blade: "pointed, veined",		color: "dark green",			texture: "coarse, stiff",	growth: "slow",	water: "frequent",		season: "cool", image_url: "#"},
+			{ name: "Zoysia",				blade: "narrow, needle-like",	color: "green",					texture: "prickly, stiff",	growth: "slow",	water: "average",		season: "warm", image_url: "#"}
+		]
+
+		let query = this.db.prepare("INSERT INTO grass_types (name, blade, color, texture, growth, water, season, image_url) VALUES (@name, @blade, @color, @texture, @growth, @water, @season, @image_url);")
+
+		this.db.transaction(grasses => {
+			console.log(grasses)
+			for (const grass of grasses) query.run(grass)
+		})
 	}
 
 	getLawn(name) {
