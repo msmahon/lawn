5068da821e157d8de08a4bc974d7cc58c1c5bbf1
Mitchell Mahoney <mahoneymitch@gmail.com>
2019-08-29 17:48:21 -0400

    Adding event components.

---
+++ client/src/components/Event.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="event-block">
+test
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Event',
+  components: {},
+  props: {
+    data: {
+      type: Object,
+      default: Object
+    }
+  },
+  data: function() {
+    return {}
+  },
+  computed: {},
+  methods: {}
+}
+</script>
+
+<style>
+.event-block {
+  width: 100%;
+  height: 100%;
+  background-color: red;
+}
+</style>
---
+++ client/src/components/EventList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="event-list">
+    <Event v-for="event in events" :data="event" :key="event.id" />
+  </div>
+</template>
+
+<script>
+import Event from './Event.vue'
+
+export default {
+  name: 'EventList',
+  components: {Event},
+  props: {},
+  data: function() {
+    return {
+      events: [
+        {id:1, type: "water", date: "2019-08-23"},
+        {id:2, type: "water", date: "2019-08-24"},
+        {id:3, type: "water", date: "2019-08-25"},
+        {id:4, type: "water", date: "2019-08-26"},
+        {id:5, type: "water", date: "2019-08-27"},
+      ]
+    }
+  },
+  computed: {},
+  methods: {}
+}
+</script>
+
+<style>
+#event-list {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
